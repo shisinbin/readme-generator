b22cb42559a97f0128257f91edeabdca7cdeaebd
ssb <sb1664@gmail.com>
2023-01-24 22:55:45 +0000

    add modularisation in which ability to grab user input is more or less done

---
+++ index.js
@@ -0,0 +1,11 @@
+const askQuestions = require('./lib/askQuestions');
+
+function init() {
+  console.log(
+    "----------------------------------------------------------------------------------\nWelcome to this README.md file generator for your amazing app! Let's get started!\n----------------------------------------------------------------------------------"
+  );
+
+  askQuestions();
+}
+
+init();
